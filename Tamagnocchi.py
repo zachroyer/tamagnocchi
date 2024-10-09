a0dc3e7b70fe1fa04d2ff25f68f665f940afba74
Zach Royer <zach.royer14@gmail.com>
2024-10-09 17:54:38 -0600

    Tamagnocchi class instantiated.

---
+++ Tamagnocchi.py
@@ -0,0 +1,8 @@
+class Tamagnocchi:
+    def __init__(self, name, food_type):
+        self.name = name
+        self.food_type = food_type
+        self.age = 0
+        self.happiness = 0
+        self.hunger = 0
+    
