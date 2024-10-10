54777a84dc26e9f6607a11467f7cceb2a7c29b69
zachroyer <zach.royer14@gmail.com>
2024-10-10 11:31:13 -0600

    Tests directory and tests for tamagnocchi created.

---
+++ tests/tamagnocchi_tests.py
@@ -0,0 +1,10 @@
+import unittest
+import Tamagnocchi
+
+class TestTamagnocchi(unittest.TestCase):
+
+    def test_tamagnocchi_constructor(self):
+        self.pet = Tamagnocchi("bob", "carrot")
+
+    
+
